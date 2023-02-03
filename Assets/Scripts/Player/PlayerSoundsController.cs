f3f057b63931210d879ec4b70830c999891bc417
Ruslan <rsmirnov.dev@gmail.com>
2023-02-03 20:40:21 +0200

    Fix jump, drop, death play sounds

--- Assets/Scripts/Player/PlayerSoundsController.cs
+++ Assets/Scripts/Player/PlayerSoundsController.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Animations;
 using UnityEngine;
 
 public class PlayerSoundsController : MonoBehaviour
@@ -11,6 +8,7 @@ public class PlayerSoundsController : MonoBehaviour
 
     public AudioClip walk;
     public AudioClip jump;
+    public AudioClip doubleJump;
     public AudioClip drop;
     public AudioClip idle;
     public AudioClip death;
@@ -25,10 +23,9 @@ public class PlayerSoundsController : MonoBehaviour
     {
         idleTick = idleSoundInterval;
 
-        Controller.PlayerControlls.Player.Jump.performed += ctx => PlayClip(jump);
-        Controller.PlayerControlls.Player.Attack.performed += ctx => PlayClip(drop);
-        //Controller.PlayerControlls.Player.Move.performed += ctx => print("move 1");
-        //Controller.PlayerControlls.Player.Move.started += ctx => print("move 2");
+        Controller.Jumped += () => PlayClip(jump);
+        Controller.DoubleJumped += () => PlayClip(doubleJump);
+        Controller.Dropped += () => PlayClip(drop);
         Health.OnDeath += () => PlayClip(death);
     }
 
