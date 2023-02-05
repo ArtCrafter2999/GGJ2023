94fe6e0e0d89ae4718654a104c8a3622f86fd678
Ruslan <rsmirnov.dev@gmail.com>
2023-02-05 14:23:38 +0200

    sound on button hover

---
+++ Assets/Scripts/HoverSound.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverSound : MonoBehaviour, IPointerEnterHandler
+{
+    public AudioClip clip;
+
+    AudioSource source;
+
+    private void Awake()
+    {
+        source = GetComponentInParent<AudioSource>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        source.PlayOneShot(clip);
+    }
+}
